8d63c65b7d9487ec06db57ba6baa6ce1a46152e1
[Jay Huang] <53930607+w286432531@users.noreply.github.com>
2021-09-16 23:13:04 -0400

    add feed and play methods.

--- VirtualPet/Pet.cs
+++ VirtualPet/Pet.cs
@@ -6,6 +6,21 @@ namespace VirtualPet
 {
     public class Pet
     {
-
+        // adding ability to play with pet. use petname.play() in program.cs to call function
+        public void play()
+        {
+            //play increase hunger by 10
+            this.Hunger += 10;
+            //play decrease boredom by 20
+            this.Boredom -= 20;
+            //play increase health by 10
+            this.Health += 10;
+        }
+        // adding ability to feed the pet. use petname.play() in program.cs to call funcion
+        public void feed()
+        {
+            //feed decrease hunger by 40
+            this.Hunger -= 40;
+        }
     }
 }
