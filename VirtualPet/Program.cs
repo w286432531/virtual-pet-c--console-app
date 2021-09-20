26b3422a839074742c9b5abebefce0cd61e91bd5
KPTousey <kevintk1969@gmail.com>
2021-09-20 16:59:46 -0400

    added shelter list pet view

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -5,13 +5,26 @@ namespace VirtualPet
 {
     class Program
     {
-        public static Pet testPet = new Pet();
+        public static Pet testPet = new Pet("test", "test");
         static void Main(string[] args)
         {
             CatPicture.PrintCatPicture();
             Timer _timer = new Timer(Tick, null, 0, 15000);
             Console.WriteLine("Hello! Welcome to Virtual Pets");
 
+
+            // Kevin and Jay 9/20----------
+            // testing the shelter list view pets
+            //Shelter myShelter = new Shelter();
+            //Pet myPet1 = new Pet("spot", "dog");
+            //Pet myPet2 = new Pet("muffy", "dog");
+            //Pet myPet3 = new Pet("charlie", "dog");
+            //Console.ReadLine();
+            //myShelter.PetList.Add(myPet1);
+            //myShelter.PetList.Add(myPet2);
+            //myShelter.PetList.Add(myPet3);
+            //myShelter.GetStatusAll();
+            //-----------------------------
         }
         public static void Tick(object o) 
         {
--- VirtualPet/Shelter.cs
+++ VirtualPet/Shelter.cs
@@ -9,5 +9,25 @@ namespace VirtualPet
 	class Shelter
 	{
 		//tbd
+		//public List <Pet> PetList { get; set; }
+		public List<Pet> PetList = new List<Pet>();
+
+
+		public Shelter()
+		{
+			
+		}
+
+		public void GetStatusAll()
+		{
+			string divider = new string('-', 60);
+			Console.Clear();
+			Console.WriteLine("Name\t\tHunger\t\tBoredom\t\tHealth");
+			Console.WriteLine(divider);
+			foreach (var pet in PetList)
+			{
+				Console.WriteLine(pet.Name + "\t\t" + pet.Hunger + "\t\t" + pet.Boredom + "\t\t" + pet.Health);
+			}
+		}
 	}
 }
